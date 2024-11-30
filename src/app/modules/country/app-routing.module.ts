dffeb13e111aac333d22d3426a2d630975eb7b8c
Diego R <diegosantisromero@gmail.com>
2024-11-30 02:52:43 -0300

    Routes link

--- src/app/modules/country/app-routing.module.ts
+++ src/app/modules/country/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { HomePageComponent } from "./shared/components/home-page/home-page.component";
 import { AboutPageComponent } from "./shared/components/about-page/about-page.component";
+import { ContactPageComponent } from "./shared/components/contact-page/contact-page.component";
 
 const routes: Routes = [
   {
@@ -16,7 +17,11 @@ const routes: Routes = [
   {
     path: 'about',
     component: AboutPageComponent
-  }
+  },
+  {
+    path: 'contact',
+    component: ContactPageComponent
+  },
 ];
 
 @NgModule({
--- src/app/modules/country/shared/shared.module.ts
+++ src/app/modules/country/shared/shared.module.ts
@@ -2,19 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutPageComponent } from './components/about-page/about-page.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [
     AboutPageComponent,
-    HomePageComponent
+    HomePageComponent,
+    SidebarComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     AboutPageComponent,
+    SidebarComponent,
     HomePageComponent
   ]
 })
